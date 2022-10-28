a61a1b2f85dbd1bab8f8e80a59eb2853c33dd756
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-28 20:05:59 +0700

    fix redux product action

--- src/services/redux/product/action/types.ts
+++ src/services/redux/product/action/types.ts
@@ -1,8 +1,14 @@
-export type TProductAction = 'GET_PRODUCT' | 'SET_PRODUCT'
+export type TProductAction =
+  | 'GET_PRODUCT'
+  | 'SET_PRODUCT'
+  | 'GET_PRODUCT_CATEGORIES'
+  | 'SET_PRODUCT_CATEGORIES'
 
 export interface IProductAction {
   GET_PRODUCT: () => void
   SET_PRODUCT: () => void
+  GET_PRODUCT_CATEGORIES: () => void
+  SET_PRODUCT_CATEGORIES: () => void
 }
 
 export interface IProductActionReturn {
