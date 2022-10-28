f22d2cf108bf503ef603b2c4811dc289bb751d52
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-28 19:31:39 +0700

    dispatch

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { productAction } from '@/services'
 
 import { router } from './helpers'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch<any>(productAction.GET_PRODUCT())
+  }, [])
+
   return (
     <HelmetProvider>
       <RouterProvider router={router} />
