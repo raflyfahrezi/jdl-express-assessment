a3887d443924e300eb6f3ba387dbec0a59596ad8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-28 19:31:20 +0700

    add new model

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
+import { IProduct } from '@/models'
 import { Wrapper } from '@/components'
 
 import './styles.scoped.scss'
 
 const Home = () => {
+  const products: IProduct[] = useSelector(
+    (state: any) => state.productReducer.products
+  )
   return (
     <Wrapper>
-      <div className='home'>Home</div>
+      <div className='home'>
+        {products.map((item) => {
+          return <p key={item.id}>{item.title}</p>
+        })}
+      </div>
     </Wrapper>
   )
 }
