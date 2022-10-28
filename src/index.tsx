b92df6c4dbf4ee54c3ca2fb930600650f8adac5a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-28 12:40:02 +0700

    import font

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from '@/app'
 
 import '@/styles/global.scss'
+
 import '@fontsource/open-sans/400.css'
+import '@fontsource/frank-ruhl-libre/400.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
