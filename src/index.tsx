e59848aade6881bc19c849b028345579fbf9f9ea
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-28 07:48:19 +0700

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/app'
 
 import '@/styles/global.scss'
+import '@fontsource/open-sans/400.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
