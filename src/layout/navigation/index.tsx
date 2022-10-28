9c25d90f586a02be90387fecce475143de0319a9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-28 09:14:51 +0700

    fix navigation

--- src/layout/navigation/index.tsx
+++ src/layout/navigation/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import { Wrapper } from '@/components'
+
+import './styles.scoped.scss'
+
 const Navigation = () => {
-  return <div>Navigation</div>
+  return (
+    <nav className='navigation'>
+      <Wrapper>
+        <div>
+          <div>
+            <p>Farhan's Shop</p>
+          </div>
+          <div>
+            <input type='text' />
+          </div>
+        </div>
+      </Wrapper>
+    </nav>
+  )
 }
 
 export default Navigation
