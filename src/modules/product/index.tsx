da6c8d77feb2f21b5a03f1062966d21c6c881fbb
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-29 00:59:11 +0700

    fix prodcut

--- src/modules/product/index.tsx
+++ src/modules/product/index.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+
+import { IProduct } from '@/models'
+import { axiosGet } from '@/utils'
+import { Wrapper, Loading } from '@/components'
+
+import './styles.scoped.scss'
 
 const Product = () => {
-  return <div>Product</div>
+  const { id } = useParams()
+
+  const [product, setProduct] = useState<IProduct>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  const fetchData = async () => {
+    const { data } = await axiosGet<IProduct>(`products/${id}`, {})
+
+    setProduct(data)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <Wrapper>
+      <div className='product'>
+        {isLoading ? (
+          <div className='product__loading'>
+            <Loading size='large' />
+          </div>
+        ) : (
+          <div>
+            <p>{product?.title}</p>
+          </div>
+        )}
+      </div>
+    </Wrapper>
+  )
 }
 
 export default Product
