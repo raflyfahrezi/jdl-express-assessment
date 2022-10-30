c276faafc61cfa92ea2fd5558e2faade9172d233
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-30 22:06:47 +0700

    remove duplicate button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
 import Tag from './tag'
 import Card from './card'
-import Button from './button'
 import Loading from './loading'
 import Wrapper from './wrapper'
 import Carousel from './carousel'
 
-export { Tag, Card, Button, Loading, Wrapper, Carousel }
+export { Tag, Card, Loading, Wrapper, Carousel }
 export * from './form'
