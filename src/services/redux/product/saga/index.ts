4c9021f81ee490ee4b696e722998649c1a5c2c7d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-30 17:57:53 +0700

    fix redux product saga

--- src/services/redux/product/saga/index.ts
+++ src/services/redux/product/saga/index.ts
@@ -1,10 +1,19 @@
 import { takeEvery } from 'redux-saga/effects'
 
-import { getProducts, getProductsCategories } from './helpers'
+import {
+  getProducts,
+  getProductByCategory,
+  getProductsCategories,
+} from './helpers'
 
-import { GET_PRODUCT, GET_PRODUCT_CATEGORIES } from '../action'
+import {
+  GET_PRODUCT,
+  GET_PRODUCT_CATEGORIES,
+  GET_PRODUCT_BY_CATEGORY,
+} from '../action'
 
 export function* productSaga() {
   yield takeEvery(GET_PRODUCT, getProducts)
   yield takeEvery(GET_PRODUCT_CATEGORIES, getProductsCategories)
+  yield takeEvery(GET_PRODUCT_BY_CATEGORY, getProductByCategory)
 }
