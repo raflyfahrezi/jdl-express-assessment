16acd21ae5631a04219311417b9176f321690e5e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-30 17:57:41 +0700

    fix redux product action

--- src/services/redux/product/action/index.ts
+++ src/services/redux/product/action/index.ts
@@ -4,6 +4,7 @@ export const GET_PRODUCT: TProductAction = 'GET_PRODUCT'
 export const SET_PRODUCT: TProductAction = 'SET_PRODUCT'
 export const GET_PRODUCT_CATEGORIES: TProductAction = 'GET_PRODUCT_CATEGORIES'
 export const SET_PRODUCT_CATEGORIES: TProductAction = 'SET_PRODUCT_CATEGORIES'
+export const GET_PRODUCT_BY_CATEGORY: TProductAction = 'GET_PRODUCT_BY_CATEGORY'
 
 export const productAction: IProductAction = {
   GET_PRODUCT: (): IProductActionReturn => {
@@ -24,6 +25,12 @@ export const productAction: IProductAction = {
   SET_PRODUCT_CATEGORIES: (): IProductActionReturn => {
     return {
       type: 'SET_PRODUCT_CATEGORIES',
+    }
+  },
+  GET_PRODUCT_BY_CATEGORY: (category: string): IProductActionReturn => {
+    return {
+      type: 'GET_PRODUCT_BY_CATEGORY',
+      data: category,
     }
   },
 }
--- src/services/redux/product/action/types.ts
+++ src/services/redux/product/action/types.ts
@@ -3,12 +3,14 @@ export type TProductAction =
   | 'SET_PRODUCT'
   | 'GET_PRODUCT_CATEGORIES'
   | 'SET_PRODUCT_CATEGORIES'
+  | 'GET_PRODUCT_BY_CATEGORY'
 
 export interface IProductAction {
   GET_PRODUCT: () => void
   SET_PRODUCT: () => void
   GET_PRODUCT_CATEGORIES: () => void
   SET_PRODUCT_CATEGORIES: () => void
+  GET_PRODUCT_BY_CATEGORY: (category: string) => void
 }
 
 export interface IProductActionReturn {
