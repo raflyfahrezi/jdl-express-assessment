446f318733b66cf13ca1b7808c63399f0caf3872
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-30 19:27:29 +0700

    adding new product state

--- src/services/redux/product/state/index.ts
+++ src/services/redux/product/state/index.ts
@@ -1,6 +1,8 @@
 import { IProductState } from '@/models'
 
 export const initialState: IProductState = {
+  keyword: '',
+  category: '',
   products: [],
   categories: [],
   isLoadingProducts: true,
