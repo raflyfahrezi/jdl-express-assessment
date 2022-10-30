4adbf8d773fd5b194a1f83fa747284e49fe44677
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-30 19:27:23 +0700

    adding new product reducer

--- src/services/redux/product/reducer/index.ts
+++ src/services/redux/product/reducer/index.ts
@@ -4,7 +4,10 @@ import { initialState } from '../state'
 import { IProductActionReturn } from '../action/types'
 import {
   SET_PRODUCT,
+  SET_PRODUCT_KEYWORD,
+  SET_PRODUCT_CATEGORY,
   SET_PRODUCT_CATEGORIES,
+  GET_PRODUCT_BY_KEYWORD,
   GET_PRODUCT_BY_CATEGORY,
 } from '../action'
 
@@ -29,6 +32,23 @@ export const productReducer = (
       return {
         ...state,
         isLoadingProducts: true,
+      }
+    case GET_PRODUCT_BY_KEYWORD:
+      return {
+        ...state,
+        category: '',
+        isLoadingProducts: true,
+      }
+    case SET_PRODUCT_KEYWORD:
+      return {
+        ...state,
+        keyword: action.data,
+      }
+    case SET_PRODUCT_CATEGORY:
+      return {
+        ...state,
+        category: action.data,
+        keyword: '',
       }
     default:
       return state
